a82a7664e82523551990cfe806656ac6e136eef7
mohadeseh <mohadesehataolahi05@gmail.com>
2024-01-09 20:28:30 +0330

    home is almost done!

--- post/urls.py
+++ post/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from .views import HomeView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+]
